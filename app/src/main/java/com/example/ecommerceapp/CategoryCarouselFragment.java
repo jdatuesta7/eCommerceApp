4f5d8bc809a78babc76e70233d307ecc13f61a14
Jhonatan <jdatuesta7@gmail.com>
2021-10-25 17:15:26 -0500

    layouts explorar y login terminados

--- app/src/main/java/com/example/ecommerceapp/CategoryCarouselFragment.java
+++ app/src/main/java/com/example/ecommerceapp/CategoryCarouselFragment.java
@@ -1,30 +1,24 @@
 package com.example.ecommerceapp;
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-import androidx.core.content.ContextCompat;
-import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
-import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DiscoverFragment#newInstance} factory method to
+ * Use the {@link CategoryCarouselFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DiscoverFragment extends Fragment {
+public class CategoryCarouselFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,8 +28,14 @@ public class DiscoverFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private CarouselView carouselView;
+    private int[] imagesList = {
+            R.drawable.pc_gamer,
+            R.drawable.xbox_one_s,
+            R.drawable.portatil_asus
+    };
 
-    public DiscoverFragment() {
+    public CategoryCarouselFragment() {
         // Required empty public constructor
     }
 
@@ -45,11 +45,11 @@ public class DiscoverFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DiscoverFragment.
+     * @return A new instance of fragment CategoryCarouselFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DiscoverFragment newInstance(String param1, String param2) {
-        DiscoverFragment fragment = new DiscoverFragment();
+    public static CategoryCarouselFragment newInstance(String param1, String param2) {
+        CategoryCarouselFragment fragment = new CategoryCarouselFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,22 +65,32 @@ public class DiscoverFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
-    }
 
-//    private Button btnNextCategory, btnPreviousCategory;
-//    private EditText etSearch;
-    private ImageCarousel carousel;
-    private List<CarouselItem> list;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_discover, container, false);
+        View root = inflater.inflate(R.layout.fragment_category_carousel, container, false);
 
-        //My backend here
+//        int width = 150, height = 150;
+//
+//        for (int image:imagesList) {
+//
+//        }
+        
+        carouselView = root.findViewById(R.id.carouselView);
+        carouselView.setPageCount(imagesList.length);
+        carouselView.setImageListener(imageListener);
 
         return root;
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(imagesList[position]);
+        }
+    };
 }
